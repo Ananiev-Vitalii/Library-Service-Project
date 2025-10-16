a295839ac5f7f4190d01c28ba1b52d264116df78
Vitalii <ananievvitalii10@gmail.com>
2025-10-16 15:18:45 -0700

    Books: add Book model, admin registration, and migrations
    - Implemented Book model (title, author, cover, inventory, daily_fee) with indexes and DB checks
    - Registered Book in Django admin with list filters/search
    - Generated and applied migrations

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from books.models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "cover", "inventory", "daily_fee")
+    list_display_links = ("id", "title")
+    list_filter = ("cover", "author")
+    search_fields = ("title", "author")
+    ordering = ("title", "author")
