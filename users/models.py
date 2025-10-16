e3398e5618f2ddc2ded3a36e5facf7c2bc54edda
Vitalii <ananievvitalii10@gmail.com>
2025-10-16 13:16:57 -0700

    Users: add custom User model with email login
    - Implemented email-based User (AbstractUser without username) + custom UserManager
    - Set AUTH_USER_MODEL = "users.User"
    - Registered model in admin with customized UserAdmin
    - Ran initial migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,57 @@
+from __future__ import annotations
 from django.db import models
+from typing import Any
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 
-# Create your models here.
+
+class UserManager(DjangoUserManager):
+    use_in_migrations = True
+
+    def _create_user(
+        self, email: str, password: str | None, **extra_fields: Any
+    ) -> User:
+        if not email:
+            raise ValueError("The email field is required")
+        email = self.normalize_email(email)
+        user: User = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+        self, email: str, password: str | None = None, **extra_fields: Any
+    ) -> User:
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email: str, password: str, **extra_fields: Any) -> User:
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    first_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self) -> str:
+        return self.email
